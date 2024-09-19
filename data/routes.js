da5affa14b140b594145dc5f2cb6b83c2ec1c4db
clav1stech <clement.roose@outlook.com>
2024-09-19 15:53:02 +0200

    Create routes.js

---
+++ data/routes.js
@@ -0,0 +1,17 @@
+// data/routes.js
+
+const routes = [
+    {
+        id: "route1",
+        name: "Trajet Nord-Sud",
+        direction: "NS", // NS: Nord vers Sud
+        pointsFile: "points_route1.js"
+    },
+    {
+        id: "route2",
+        name: "Trajet Sud-Nord",
+        direction: "SN", // SN: Sud vers Nord
+        pointsFile: "points_route2.js"
+    }
+    // Ajoutez d'autres trajets ici
+];
