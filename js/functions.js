ad9cafca19271fd834b181ee6d8dd640df1991be
clav1stech <clement.roose@outlook.com>
2024-09-19 15:54:50 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -30,3 +30,24 @@ function showError(error) {
     }
 }
 
+// Fonction pour charger dynamiquement un fichier JavaScript
+function loadScript(url, callback) {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+
+    if (script.readyState){  // Pour IE
+        script.onreadystatechange = function(){
+            if (script.readyState === "loaded" || script.readyState === "complete"){
+                script.onreadystatechange = null;
+                if (callback) callback();
+            }
+        };
+    } else {  // Pour les autres navigateurs
+        script.onload = function(){
+            if (callback) callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
