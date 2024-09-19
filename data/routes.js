bafcaace949640bb4942759844d0271b2256f0a5
clav1stech <clement.roose@outlook.com>
2024-09-19 17:53:29 +0200

    Update routes.js

--- data/routes.js
+++ data/routes.js
@@ -4,13 +4,13 @@ const trajets = [
     {
         id: "PARMRS",
         name: "Paris - Marseille",
-        direction: "south-north", // 'north-south' ou 'south-north'
+        direction: "NS", // 'north-south' ou 'south-north'
         pointsFile: "points_route1.json" // Utiliser .json si vous avez converti en JSON
     },
     {
         id: "MRSPAR",
         name: "Marseille - Paris",
-        direction: "south-north",
+        direction: "SN",
         pointsFile: "points_route2.json" // Utiliser .json si vous avez converti en JSON
     }
     // Ajoutez d'autres trajets ici si nécessaire
