8ed509b66bb7518bf0854ac3c726497d27e1a01e
clav1stech <clement.roose@outlook.com>
2024-09-19 15:53:32 +0200

    Create points_route1.js

---
+++ data/points_route1.js
@@ -0,0 +1,11 @@
+// data/points_route1.js
+
+const pointsRoute1 = [
+    { name: "Paris-Lyon", PK: 0.000, duree: 0, lat: 48.84431, lon: 2.37564, retard: 0 },
+    { name: "Bif. de Créteil", PK: 9.326, duree: 350, lat: 48.77490, lon: 2.43626, retard: 0 },
+    { name: "Tunnel de Limeil-Brevannes", PK: 4.117, duree: 142, lat: 48.74884, lon: 2.47240, retard: 0 },
+    { name: "Tranchée couverte de Villecresne", PK: 7.633, duree: 118, lat: 48.73355, lon: 2.50744, retard: 0 },
+    { name: "Bif. de Chevry-Cossigny", PK: 21.486, duree: 300, lat: 48.70186, lon: 2.68159, retard: 0 },
+    // ... Ajoutez tous les autres points pour ce trajet
+    { name: "Marseille Saint-Charles", PK: 861.800, duree: 360, lat: 43.30334, lon: 5.38096, retard: 0 },
+];
